b4a95a9b861b9b20d018fee4782bfb4a9a4ab640
shony853 <112523485+shony853@users.noreply.github.com>
2023-04-21 11:27:06 -0400

    project3

--- projects/project3/project3.js
+++ projects/project3/project3.js
@@ -1,5 +1,12 @@
-const appToken= "sEEOQS7Ai1EdS0XHVlDTsf53W"
-const url= "https://data.cityofnewyork.us/resource/c3uy-2p5r.json"
+const appToken = "sEEOQS7Ai1EdS0XHVlDTsf53W";
+const url = "https://data.cityofnewyork.us/resource/c3uy-2p5r.json?geo_type_name=CD&$select=geo_place_name";
 
-  
-  
+const geoplaceElement = document.querySelector(".geoplace");
+
+fetch(url, { headers: { "X-App-Token": appToken } })
+  .then(response => response.json())
+  .then(data => {
+    const geoPlaceNames = data.map(item => item.geo_place_name);
+    geoplaceElement.textContent = geoPlaceNames.join(", ");
+  })
+  .catch(error => console.error(error));
