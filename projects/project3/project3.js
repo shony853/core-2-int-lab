cc037f0a910e0ca8c13a0f801fc8f1fe4fcdc76d
shony853 <112523485+shony853@users.noreply.github.com>
2023-05-02 20:31:26 -0400

    project3

--- projects/project3/project3.js
+++ projects/project3/project3.js
@@ -6,37 +6,46 @@ aboutElement.addEventListener("click", () => {
 });
 
 const appToken = "sEEOQS7Ai1EdS0XHVlDTsf53W";
-const boroughUrl = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?geo_type_name=Borough&$select=geo_place_name`;
-const citywideUrl = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?geo_type_name=Citywide&$select=geo_place_name`;
+const cdUrl = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?time_period=Summer 2020&geo_type_name=CD&$select=geo_place_name`;
+const boroughUrl = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?time_period=Summer 2020&geo_type_name=Borough&$select=geo_place_name`;
+const citywideUrl = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?time_period=Summer 2020&geo_type_name=Citywide&$select=geo_place_name`;
+const allData = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?time_period=Summer 2020`;
 
+let localData;
 const geoplaceElement = document.querySelector(".geoplace");
-const cdLabelElement = document.querySelector(".cdlabel");
+const outputGeoplacenameElement = document.querySelector(".output_geoplacename");
 
 function displayGeoPlaceName(url) {
   fetch(url, { headers: { "X-App-Token": appToken } })
     .then(response => response.json())
     .then(data => {
-      const uniqueGeoPlaceNames = [...new Set(data.map(item => item.geo_place_name))];
-      const geoPlaceNameHtml = uniqueGeoPlaceNames.map(name => `<p>${name}</p>`).join("");
-      geoplaceElement.innerHTML = geoPlaceNameHtml;
-    })
-    .catch(error => console.error(error));
+      geoplaceElement.innerHTML = "";
+      data.forEach(item => {
+        const placeElement = document.createElement("div");
+        placeElement.classList.add("geoplacename");
+        placeElement.textContent = item.geo_place_name;
+        placeElement.addEventListener("click", () => {
+          outputGeoplacenameElement.textContent = item.geo_place_name;
+        });
+        geoplaceElement.appendChild(placeElement);
+      });
+    });
 }
 
-const boroughElement = document.querySelector(".borough");
-boroughElement.addEventListener("click", () => displayGeoPlaceName(boroughUrl));
-
-const citywideElement = document.querySelector(".citywide");
-citywideElement.addEventListener("click", () => displayGeoPlaceName(citywideUrl));
+displayGeoPlaceName(cdUrl);
 
 const cdElement = document.querySelector(".cd");
+const boroughElement = document.querySelector(".borough");
+const citywideElement = document.querySelector(".citywide");
+
 cdElement.addEventListener("click", () => {
-  cdLabelElement.style.display = "block";
+  displayGeoPlaceName(cdUrl);
 });
 
-const toggleElement = document.querySelector(".toggle");
-const bodyElement = document.querySelector("body");
+boroughElement.addEventListener("click", () => {
+  displayGeoPlaceName(boroughUrl);
+});
 
-toggleElement.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark-mode");
+citywideElement.addEventListener("click", () => {
+  displayGeoPlaceName(citywideUrl);
 });
