b05587161cf7101a7fffdbbef7f6cac5fae794c6
shony853 <112523485+shony853@users.noreply.github.com>
2023-05-04 17:53:56 -0400

    project3

--- projects/project3/project3.js
+++ projects/project3/project3.js
@@ -1,8 +1,11 @@
 const aboutElement = document.querySelector(".about");
 const aboutDetailElement = document.querySelector(".aboutdetail");
-
+const geoplaceElement = document.querySelector(".geoplace");
+const outputNameElement = document.querySelector(".output_name");
+const detailElement = document.querySelector(".detail")
 aboutElement.addEventListener("click", () => {
   aboutDetailElement.style.display = aboutDetailElement.style.display === "none" ? "block" : "none";
+  detailElement.style.display = "none";
 });
 
 const appToken = "sEEOQS7Ai1EdS0XHVlDTsf53W";
@@ -13,8 +16,16 @@ const allData = `https://data.cityofnewyork.us/resource/c3uy-2p5r.json?time_peri
 
 
 let localData;
-const geoplaceElement = document.querySelector(".geoplace");
-const outputGeoplacenameElement = document.querySelector(".output_geoplacename");
+
+
+geoplaceElement.addEventListener("click", () => {
+  detailElement.style.display = "block";
+  outputNameElement.style.display = "block";
+  aboutDetailElement.style.display = "none";
+});
+// const outputGeoplacenameElement = document.querySelector(".output_geoplacename");
+
+
 
 // function displayGeoPlaceName(url) {
 //   fetch(url, { headers: { "X-App-Token": appToken } })
@@ -52,7 +63,7 @@ const citywideElement = document.querySelector(".citywide");
 
 let boroughs = {};
 let districts = {};
-let citywide = 0;
+let city = {};
 parseData();
 function parseData(){
   fetch(allData, { headers: { "X-App-Token": appToken } })
@@ -67,27 +78,52 @@ function parseData(){
         districts[ record.geo_place_name ] = record.data_value;
       }
       if( record.geo_type_name == 'Citywide'){
-        citywide = record.data_value;
+        city[ record.geo_place_name ] = record.data_value;
       }
     });
 
     console.log(boroughs);
     console.log(districts);
-    console.log(citywide);
+    console.log(city);
   });
 }
 
 const max = 33.51;
 const min = 25.56;
 const outputContainer = document.querySelector('.output_datavalue');
+cdElement.addEventListener("click", () => {
+  // displayGeoPlaceName(boroughUrl);
+  geoplaceElement.innerHTML = "";
+  for (const cd in districts){
+    let newItem = document.createElement("div");
+    newItem.classList.add("cd-option");
+    newItem.innerText = cd;
+
+    geoplaceElement.appendChild( newItem );
+    newItem.addEventListener('click', function(){
+      let geoplaceName = document.querySelector(".output_geoplacename");
+      geoplaceName.innerHTML= cd;
+      let value = districts[cd];
+      outputContainer.innerText = value;
+      let ratio = (parseFloat(value)-min) / (max - min);
+      let weight = Math.round(1 + ratio*899);
+      console.log(weight);
+      outputContainer.style.setProperty('--wght', weight);
+    });
+  }
+});
 boroughElement.addEventListener("click", () => {
   // displayGeoPlaceName(boroughUrl);
+  geoplaceElement.innerHTML = "";
   for (const borough in boroughs){
     let newItem = document.createElement("div");
     newItem.classList.add("borough-option");
     newItem.innerText = borough;
     geoplaceElement.appendChild( newItem );
     newItem.addEventListener('click', function(){
+      let geoplaceName = document.querySelector(".output_geoplacename");
+      geoplaceName.innerHTML= borough;
+
       let value = boroughs[borough];
       outputContainer.innerText = value;
       let ratio = (parseFloat(value)-min) / (max - min);
@@ -97,4 +133,27 @@ boroughElement.addEventListener("click", () => {
     });
   }
 });
+citywideElement.addEventListener("click", () => {
+  // displayGeoPlaceName(boroughUrl);
+  geoplaceElement.innerHTML = "";
+  for (const citywide in city){
+    let newItem = document.createElement("div");
+    newItem.classList.add("citywide-option");
+    newItem.innerText = citywide;
+    geoplaceElement.appendChild( newItem );
+    newItem.addEventListener('click', function(){
+      let geoplaceName = document.querySelector(".output_geoplacename");
+      geoplaceName.innerHTML= citywide;
+      let value = city[citywide];
+      outputContainer.innerText = value;
+      let ratio = (parseFloat(value)-min) / (max - min);
+      let weight = Math.round(1 + ratio*899);
+      console.log(weight);
+      outputContainer.style.setProperty('--wght', weight);
+    });
+  }
+});
+
+
+
 
